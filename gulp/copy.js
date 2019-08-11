5d1c3f032fb004c072928ea0e0aefda16abb9441
uzinok <uzinok@yandex.ru>
2019-08-11 14:41:01 +0400

    copy image .ico

--- gulp/copy.js
+++ gulp/copy.js
@@ -3,7 +3,7 @@ var gulp = require("gulp");
 gulp.task("copy", function () {
   return gulp.src([
     "src/fonts/*.{woff, woff2}*",
-    "src/img/*.+(png|jpg|svg|webp)*",
+    "src/img/*.+(png|jpg|svg|webp|ico)*",
   ], {
       base: "src"
     })
