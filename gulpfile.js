450fb2bf172fb5fa65857b28b8e65ac5fb09b5e7
uzinok_HP <uzinok@yandex.ru>
2021-12-23 08:08:19 +0300

    добавляет grid.less

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 const {
-	src,
-	dest,
-	watch,
-	series,
-	parallel
+    src,
+    dest,
+    watch,
+    series,
+    parallel
 } = require('gulp');
 
 // clean
@@ -33,128 +33,138 @@ const htmlmin = require('gulp-htmlmin');
 const nunjucks = require('gulp-nunjucks');
 
 const paths = {
-	clean: 'dest',
-	styles: {
-		src: 'src/styles/*.less',
-		watch: 'src/styles/**/*.less',
-		dest: 'dest/styles/'
-	},
-	scripts: {
-		src: 'src/scripts/**/*.js',
-		watch: 'src/scripts/**/*.js',
-		dest: 'dest/scripts/'
-	},
-	html: {
-		src: 'src/**/*.html',
-		watch: 'src/**/*.html',
-		dest: 'dest/'
-	}
+    clean: 'dest',
+    styles: {
+        src: 'src/styles/*.less',
+        watch: 'src/styles/**/*.less',
+        dest: 'dest/styles/'
+    },
+    scripts: {
+        src: 'src/scripts/**/*.js',
+        watch: 'src/scripts/**/*.js',
+        dest: 'dest/scripts/'
+    },
+    html: {
+        src: 'src/**/*.html',
+        watch: 'src/**/*.html',
+        dest: 'dest/'
+    }
+}
+
+const onError = function(err) {
+    notify.onError({
+        title: "Gulp",
+        subtitle: "Failure!",
+        message: "Error: <%= error.message %>",
+        sound: "Beep"
+    })(err);
+    this.emit('end');
 }
 
 // clean
 function clean() {
-	return del(paths.clean);
+    return del(paths.clean);
 }
 
 // copy
 function copy() {
-	return src([
-			"./src/fonts/*.{woff2,woff}",
-			"./src/*.ico",
-			"./src/img/**/*.{svg,jpg,jpeg,png,webp,avif}",
-			"./src/favicons/*",
-			"./src/*.webmanifest"
-		], {
-			base: "./src"
-		})
-		.pipe(dest("./dest"));
+    return src([
+            "./src/fonts/*.{woff2,woff}",
+            "./src/*.ico",
+            "./src/img/**/*.{svg,jpg,jpeg,png,webp,avif}",
+            "./src/favicons/*",
+            "./src/*.webmanifest"
+        ], {
+            base: "./src"
+        })
+        .pipe(dest("./dest"));
 }
 
 // styles
 function styles() {
-	return src(paths.styles.src)
-		.pipe(plumber({
-			errorHandler: notify.onError(function(err) {
-				return {
-					title: 'Task styles',
-					message: "Error: <%= error.message %>",
-					sound: true
-				}
-			})
-		}))
-		.pipe(sourcemaps.init())
-		.pipe(plumber({
-			errorHandler: onError
-		}))
-		.pipe(less())
-		.pipe(gcmq())
-		.pipe(cleanCSS())
-		.pipe(autoprefixer({
-			overrideBrowserslist: ['last 2 versions']
-		}))
-		.pipe(sourcemaps.write('.'))
-		.pipe(rename({
-			basename: 'main',
-			suffix: '.min'
-		}))
-		.pipe(dest(paths.styles.dest))
-		.pipe(browserSync.stream());
+    return src(paths.styles.src)
+        .pipe(plumber({
+            errorHandler: notify.onError(function(err) {
+                return {
+                    title: 'Task styles',
+                    message: "Error: <%= error.message %>",
+                    sound: true
+                }
+            })
+        }))
+        .pipe(sourcemaps.init())
+        .pipe(plumber({
+            errorHandler: onError
+        }))
+        .pipe(less())
+        .pipe(gcmq())
+        .pipe(cleanCSS())
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 2 versions']
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(rename({
+            basename: 'main',
+            suffix: '.min'
+        }))
+        .pipe(dest(paths.styles.dest))
+        .pipe(browserSync.stream());
 }
 
 // scripts
 function scripts() {
-	return src(paths.scripts.src)
-		.pipe(plumber({
-			errorHandler: notify.onError(function(err) {
-				return {
-					title: 'Task scripts',
-					message: "Error: <%= error.message %>",
-					sound: true
-				}
-			})
-		}))
-		.pipe(babel({
-			presets: ['@babel/preset-env']
-		}))
-		.pipe(minify({
-			ext: {
-				src: '.js',
-				min: '.min.js'
-			},
-			exclude: ['tasks']
-		}))
-		.pipe(dest(paths.scripts.dest))
-		.pipe(browserSync.stream());
+    return src(paths.scripts.src)
+        .pipe(plumber({
+            errorHandler: notify.onError(function(err) {
+                return {
+                    title: 'Task scripts',
+                    message: "Error: <%= error.message %>",
+                    sound: true
+                }
+            })
+        }))
+        .pipe(babel({
+            presets: ['@babel/preset-env']
+        }))
+        .pipe(minify({
+            ext: {
+                src: '.js',
+                min: '.min.js'
+            },
+            exclude: ['tasks']
+        }))
+        .pipe(dest(paths.scripts.dest))
+        .pipe(browserSync.stream());
 }
 
 // html
 function html() {
-	return src(paths.html.src)
-		.pipe(nunjucks.compile())
-		.pipe(htmlmin({
-			removeComments: false,
-			collapseWhitespace: true
-		}))
-		.pipe(dest(paths.html.dest))
-		.pipe(browserSync.stream());
+    return src(paths.html.src)
+        .pipe(nunjucks.compile())
+        .pipe(htmlmin({
+            removeComments: false,
+            collapseWhitespace: true
+        }))
+        .pipe(dest(paths.html.dest))
+        .pipe(browserSync.stream());
 }
 
 // watch
 function watchFiles() {
-	watch(paths.styles.watch, styles)
-	watch(paths.scripts.watch, scripts)
-	watch(paths.html.watch, html)
+    watch(paths.styles.watch, styles)
+    watch(paths.scripts.watch, scripts)
+    watch(paths.html.watch, html)
 }
 
 // server
 function server() {
-	browserSync.init({
-		server: {
-			baseDir: './dest/'
-		}
-	});
+    browserSync.init({
+        server: {
+            baseDir: './dest/'
+        }
+    });
 
-	watchFiles();
+    watchFiles();
 }
 
 // clean
@@ -188,53 +198,53 @@ const ttf2woff = require('gulp-ttf2woff');
 
 // img
 function optiImg() {
-	return src("./src/img/**/*.{png,jpg}", {
-			base: 'src'
-		})
-		.pipe(squoosh())
-		.pipe(dest("src/"));
+    return src("./src/img/**/*.{png,jpg}", {
+            base: 'src'
+        })
+        .pipe(squoosh())
+        .pipe(dest("src/"))
 }
 
 function createWebp() {
-	return src("./src/resource/img/**/*.{jpg,png}")
-		.pipe(
-			squoosh({
-				webp: {}
-			})
-		)
-		.pipe(dest("./src/img"));
+    return src("./src/resource/img/**/*.{jpg,png}")
+        .pipe(
+            squoosh({
+                webp: {}
+            })
+        )
+        .pipe(dest("./src/img"));
 }
 
 function createAvif() {
-	return src("./src/resource/img/**/*.{jpg,png}")
-		.pipe(
-			squoosh({
-				avif: {}
-			})
-		)
-		.pipe(dest("./src/img"));
+    return src("./src/resource/img/**/*.{jpg,png}")
+        .pipe(
+            squoosh({
+                avif: {}
+            })
+        )
+        .pipe(dest("./src/img"));
 }
 
 function sprite() {
-	return src("./src/resource/svg/*.svg")
-		.pipe(svgSprite({
-			mode: {
-				stack: {
-					sprite: "../sprite.svg"
-				}
-			},
-		}))
-		.pipe(dest("./src/img"));
+    return src("./src/resource/svg/*.svg")
+        .pipe(svgSprite({
+            mode: {
+                stack: {
+                    sprite: "../sprite.svg"
+                }
+            },
+        }))
+        .pipe(dest("./src/img"));
 }
 
 // fonts
 function fonts() {
-	src(['src/resource/fonts/*.ttf'])
-		.pipe(ttf2woff())
-		.pipe(dest('src/fonts/'));
-	return src(['src/resource/fonts/*.ttf'])
-		.pipe(ttf2woff2())
-		.pipe(dest('src/fonts/'));
+    src(['src/resource/fonts/*.ttf'])
+        .pipe(ttf2woff())
+        .pipe(dest('src/fonts/'));
+    return src(['src/resource/fonts/*.ttf'])
+        .pipe(ttf2woff2())
+        .pipe(dest('src/fonts/'));
 }
 
 // createWebp
